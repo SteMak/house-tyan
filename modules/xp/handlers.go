63448f5ceedda3ce28385893bddc84bc62a3aef7
chest <stepan.sumkin@gmail.com>
2020-04-03 22:08:25 +0300

    correct custom to public func

--- modules/xp/handlers.go
+++ modules/xp/handlers.go
@@ -3,11 +3,12 @@ package xp
 import (
 	"fmt"
 
+	"github.com/SteMak/house-tyan/util"
 	"github.com/bwmarrin/discordgo"
 )
 
 func (bot *module) handlerXpMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if isntRightChannel(m.ChannelID, bot.config.Channels.XpFarming) {
+	if !util.EqualAny(m.ChannelID, bot.config.Channels.XpFarming) {
 		return
 	}
 	if len(m.Content) < bot.config.XpConfig.XpMesLen {
