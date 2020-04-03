6a62a1bb0fbea742d4588fd1c41fdc66e6e17c2e
chest <stepan.sumkin@gmail.com>
2020-04-04 01:56:39 +0300

    normalize config

--- modules/xp/config.go
+++ modules/xp/config.go
@@ -1,23 +1,18 @@
 package xp
 
-type channels struct {
-	XpFarming []string `yaml:"xp_farming,omitempty"`
+type messageFarm struct {
+	Channels      []string `yaml:"channels,omitempty"`
+	XpForMessage  int      `yaml:"xp_for_message,omitempty"`
+	MessageLength int      `yaml:"message_length,omitempty"`
 }
 
-type xpConfig struct {
-	XpBoost   int `yaml:"xp_boost,omitempty"`
-	XpMessage int `yaml:"xp_message,omitempty"`
-	XpVoice   int `yaml:"xp_voice,omitempty"`
-	XpMesLen  int `yaml:"xp_mes_len,omitempty"`
-	XpVoiSec  int `yaml:"xp_voi_sec,omitempty"`
-}
-
-type roles struct {
-	Hermit string `yaml:"hermit,omitempty"`
+type voiceFarm struct {
+	WaitFor    int `yaml:"wait_for,omitempty"`
+	XpForVoice int `yaml:"xp_for_voice,omitempty"`
 }
 
 type config struct {
-	Channels channels `yaml:"channels,omitempty"`
-	Roles    roles    `yaml:"roles,omitempty"`
-	XpConfig xpConfig `yaml:"xp_config,omitempty"`
+	RoleHermit  string      `yaml:"role_hermit,omitempty"`
+	MessageFarm messageFarm `yaml:"message_farm,omitempty"`
+	VoiceFarm   voiceFarm   `yaml:"voice_farm,omitempty"`
 }
