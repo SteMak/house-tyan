84d1cf3506d2ed8bbc73bdd7320a926e7570cc12
chest <stepan.sumkin@gmail.com>
2020-04-06 01:24:29 +0300

    muted dont get xp && giving xp for symbols

--- modules/xp/config.go
+++ modules/xp/config.go
@@ -6,13 +6,15 @@ import (
 
 type messageFarm struct {
 	Channels      []string `yaml:"channels,omitempty"`
+	XpForChar     float32  `yaml:"xp_for_char,omitempty"`
 	XpForMessage  int      `yaml:"xp_for_message,omitempty"`
 	MessageLength int      `yaml:"message_length,omitempty"`
 }
 
 type voiceFarm struct {
-	WaitFor    time.Duration `yaml:"wait_for,omitempty"`
-	XpForVoice int           `yaml:"xp_for_voice,omitempty"`
+	WaitFor      time.Duration `yaml:"wait_for,omitempty"`
+	XpForVoice   int           `yaml:"xp_for_voice,omitempty"`
+	MaxRoomBoost int           `yaml:"max_room_boost,omitempty"`
 }
 
 type config struct {
