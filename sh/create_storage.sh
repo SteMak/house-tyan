e39e2b8e10153d2d82672fb6fcd9b6f54ab4f0ee
cam-per <minkotaras@gmail.com>
2020-04-06 21:27:14 +0300

    docker postgres

--- sh/create_storage.sh
+++ sh/create_storage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker rm -fv anihouse-postgres
-# docker volume create anihouse-pgdata
-docker create --name anihouse-postgres -e POSTGRES_PASSWORD=password -p 54320:5432 postgres:11.5
+docker volume create anihouse-pgdata
+docker create --name anihouse-postgres -e POSTGRES_USER=bot -e POSTGRES_PASSWORD=password -e POSTGRES_DB=anihouse -p 54320:5432 postgres:11.5
