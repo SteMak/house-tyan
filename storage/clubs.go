91fff97993089c57a329e6c16b40651f95e54fc4
cam-per <minkotaras@gmail.com>
2020-05-13 17:04:02 +0300

    Club.HasMember

--- storage/clubs.go
+++ storage/clubs.go
@@ -52,7 +52,7 @@ func (c *Club) DeleteMember(tx *sqlx.Tx, memberID string) error {
 
 func (c *Club) HasMember(memberID string) (result bool, err error) {
 	err = db.Get(&result, `SELECT EXISTS(SELECT 1 FROM club_members WHERE user_id = $1)`, memberID)
-	return result, err
+	return
 }
 
 func (c *Club) Delete(tx *sqlx.Tx) error {
