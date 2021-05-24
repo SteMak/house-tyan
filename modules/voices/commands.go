4b45e52e9c8435a74021d33e26e2bd1e0b027118
ExLin <melis011102@gmail.com>
2021-05-25 01:51:36 +0600

    added unlock, lock, show, hide commands

--- modules/voices/commands.go
+++ modules/voices/commands.go
@@ -1,1 +1,75 @@
 package voices
+
+import (
+	
+	"github.com/SteMak/house-tyan/libs/dgutils"
+//	"github.com/SteMak/house-tyan/modules"
+//	"github.com/SteMak/house-tyan/out"
+//	"github.com/SteMak/house-tyan/storage"
+//	"github.com/SteMak/house-tyan/util"
+)
+
+var (
+	commands = map[string] interface{}{
+		"voice": &dgutils.Group{
+			Commands: map[string]interface{}{
+
+				"unlock": &dgutils.Command{
+					Raw: false,
+					Description: "Открыть доступ к каналу пользователю или роли",
+					Handlers: []func(*dgutils.MessageContext){
+						_module.middlewareChannel,
+					},
+					Function: _module.onVoiceUnlock,
+				},
+				
+				"lock": &dgutils.Command{
+					Raw:false,
+					Description: "Закрыть доступ к каналу пользователю или роли",
+					Handlers: []func(*dgutils.MessageContext){
+						_module.middlewareChannel,
+					},
+					Function: _module.onVoiceLock,
+				},
+
+				"show": &dgutils.Command{
+					Raw: false,
+					Description: "Показать канал для пользователя или роли",
+					Handlers: []func(*dgutils.MessageContext){
+						_module.middlewareChannel,
+					},
+					Function: _module.onVoiceShow,
+				},
+				
+				"hide": &dgutils.Command{
+					Raw: false,
+					Description: "Скрыть канал от пользователя или роли",
+					Handlers: []func(*dgutils.MessageContext){
+						_module.middlewareChannel,
+					},
+					Function: _module.middlewareChannel,
+				},
+			},
+		},
+	}
+)
+
+func (bot *module) onVoiceUnlock(ctx *dgutils.MessageContext) {
+	setPermissions(ctx, permConnect, true)
+}
+
+func (bot *module) onVoiceLock(ctx *dgutils.MessageContext) {
+	setPermissions(ctx, permConnect, false)
+}
+
+func (bot *module) onVoiceShow(ctx *dgutils.MessageContext) {
+	setPermissions(ctx, permView, true)
+}
+
+func (bot *module) onVoiceHide(ctx *dgutils.MessageContext) {
+	setPermissions(ctx, permView, false)
+}
+
+func (bot *module) onVoiceInfo(ctx *dgutils.MessageContext) {
+	
+}
